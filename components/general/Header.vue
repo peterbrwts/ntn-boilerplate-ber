c6ab785603e3a4155ef43ecb8d5e7c1fb4d34095
peterbrwts <50912949+peterbrwts@users.noreply.github.com>
2021-01-31 22:31:27 +0100

    edit navbar

--- components/general/Header.vue
+++ components/general/Header.vue
@@ -1,70 +1,68 @@
 <template>
-<header class="py-6">
-    <div class="sm:mx-auto flex flex-wrap">
-      <div class="flex-1 flex justify-between items-center">
-      <a href="#">
-        NTN
-    </a>
-  </div>
-      <!-- Desktop nav list -->
-     <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
-    <nav>
-      <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0 lg:mx-5">
-      <li class="flex-1 mr-2">
-        <nuxt-link class="btn block lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" to="/">Home</nuxt-link>
-      </li>
-      <li class="flex-1 mr-2">
-        <nuxt-link class="btn block lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" to="/blog">Blog</nuxt-link>
-      </li>
-      <li class="flex-1">
-        <nuxt-link class="btn block lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" to="/projects">Projects</nuxt-link>
-      </li>
-    </ul>
-
-    </nav>
-
-  </div>
-      <!-- Toggle menu mobile icon -->
-      <div class="ml-auto sm:hidden p-6" @click="menu = !menu">
-        menu
-      </div>
-    </div>
-    <!-- Mobile nav list -->
-    <nav v-show="menu" class="w-full">
-      <ul class="flex flex-col text-center">
-        <li
-          v-for="(item, index) in items"
-          :key="index"
+ <header class="border-t-14 border-green-700">
+      <nav class="mx-auto flex flex-wrap justify-between items-center py-8">
+        <div>
+          <nuxt-link v-if="theme === 'theme-light'" to="/">LOGO</nuxt-link>
+          <nuxt-link v-else to="/">LOGO</nuxt-link>
+        </div>
+        <div class="block lg:hidden">
+          <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
+            <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
+          </button>
+        </div>
+        <ul
+          class="uppercase tracking-wide font-bold w-full block flex-grow lg:space-x-8 space-y-6 lg:space-y-0 lg:flex lg:flex-initial lg:w-auto items-center mt-8 lg:mt-0"
+          :class="isOpen ? 'block': 'hidden'"
         >
-          <a :href="item.href" class="hover:bg-green-500 p-6 block">{{ item.title }}</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+          <li class="mb-6 lg:mb-0">
+            <!-- <search-input /> -->
+          </li>
+          <li>
+            <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-copy-primary hover:text-gray-600">Link</a>
+            <nuxt-link v-else to="/#projects" v-scroll-to="'/#projects'" class="text-copy-primary hover:text-gray-600">Link</nuxt-link>
+          </li>
+          <li>
+            <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-copy-primary hover:text-gray-600">Link</a>
+            <nuxt-link v-else to="/#about" v-scroll-to="'/#about'" class="text-copy-primary hover:text-gray-600">Link</nuxt-link>
+          </li>
+          <li>
+            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-copy-primary hover:text-gray-600">Link</a>
+            <nuxt-link v-else to="/#contact" v-scroll-to="'/#contact'" class="text-copy-primary hover:text-gray-600">Link</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/blog" class="text-copy-primary hover:text-gray-600">Link</nuxt-link>
+            <!-- <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link> -->
+          </li>
+        </ul>
+      </nav>
+    </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  components: {
+    
+  },
+  mounted() {
+  
+  },
+  data() {
+    return {
+      isOpen: false,
+      theme: '',
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    },
+    updateTheme(theme) {
+      this.theme = theme
+    }
+  }
 }
 </script>
 
 <style lang="postcss" scoped>
- #menu-toggle:checked + #menu {
-        display: block;
-      }
-.nuxt-link-exact-active {
-  @apply text-accent-100 bg-accent bg-opacity-20;
-  &:hover {
-    @apply text-white bg-accent-600;
-  }
-}
-.light-mode {
-  & .nuxt-link-exact-active {
-    @apply text-accent-600;
-    &:hover {
-      @apply text-white;
-    }
-  }
-}
+ 
 </style>
