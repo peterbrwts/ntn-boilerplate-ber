f8a42848c484b5aab6746a54cc194f6ca831930b
Berwouts <peterberwouts@icloud.com>
2021-06-30 10:26:14 +0200

    major update

--- components/global/ColorModePicker.vue
+++ components/global/ColorModePicker.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="fixed bottom-0 left-0 m-3">
-    <component :is="`icon-${color}`" @click="changeColorMode" @keypress.enter="changeColorMode" title="Toggle background color" tabindex="0" />
+    <component
+      :is="`icon-${color}`"
+      @click="changeColorMode"
+      @keypress.enter="changeColorMode"
+      title="Toggle background color"
+      tabindex="0"
+    />
   </div>
 </template>
 
@@ -15,11 +21,11 @@ export default {
   components: {
     IconSystem,
     IconLight,
-    IconDark
+    IconDark,
   },
   data() {
     return {
-      color: COLOR_MODE_FALLBACK
+      color: COLOR_MODE_FALLBACK,
     }
   },
   watch: {
@@ -34,8 +40,8 @@ export default {
         } else {
           this.color = COLOR_MODE_FALLBACK
         }
-      }
-    }
+      },
+    },
   },
   methods: {
     changeColorMode() {
@@ -52,8 +58,8 @@ export default {
         default:
           return (this.$colorMode.preference = COLOR_MODE_FALLBACK)
       }
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -74,7 +80,7 @@ export default {
     color: var(--color-primary);
   }
 }
-.light-mode {
+.light {
   & .icon {
     background-color: var(--bg);
     border-color: var(--border-color-secondary);
@@ -86,5 +92,4 @@ export default {
     }
   }
 }
-
 </style>
--- content/blog/2021-03-28-aan-de-slag-met-nuxt copy 4.md
+++ content/blog/2021-03-28-aan-de-slag-met-nuxt copy 4.md
@@ -1,5 +1,6 @@
 ---
 title: Aan de slag met Nuxt
+createdAt: 2021-02-28
 ---
 
 `Ontwikkelaars maken zich vaak zorgen over de SEO van hun SPA's (Single Page Applications) en hoe goed deze het zouden doen in Google zoekopdrachten (blogs, portfolio websites, product websites, enzovoort). Soms maken ze zich ook zorgen over hoe complex het bouwen van een server-side rendered applicatie zou kunnen zijn. In deze tutorial leren we hoe u server-side rendered applicaties kunt maken met Nuxt.js, hoe u uw applicatie kunt configureren voor ontwikkeling, en hoe u deze kunt implementeren op Netlify.`
